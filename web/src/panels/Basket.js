474530bc5858b0b4c80327679774eb30b0b2e983
Fairwale <lantsovyaroslav@gmail.com>
2020-09-09 22:23:28 +0300

    Order can be ordered with 0 price fix

--- web/src/panels/Basket.js
+++ web/src/panels/Basket.js
@@ -147,9 +147,15 @@ const Basket = ({ match: { params: { areaId, itemId }}, foodAreas, order }) => {
         </div>
       </div>
       <footer className="Place__footer">
-        <Link to={`/order/${area.id}/${item.id}`} className="Place__order">
-          Оплатить {price}
-        </Link>
+          <Link
+              to={() => {
+                  if (parseInt(price)) {
+                      return `/order/${area.id}/${item.id}`;
+                  }
+              }}
+              className="Place__order">
+              Оплатить {price}
+          </Link>;
       </footer>
     </div>
   );
